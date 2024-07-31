efce47e2e253721a4f9ccc7bddcd8e67cbdd8b9c
maksim.vasenkov@eteron.ru <maksim.vasenkov@eteron.ru>
2024-07-31 18:46:35 +0300

    потихоньку поехали

---
+++ all_candles.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
+
+from tinkoff.invest import CandleInterval, Client
+from tinkoff.invest.schemas import CandleSource
+from tinkoff.invest.utils import now
+load_dotenv()
+
+TOKEN = os.environ["PROD_INVEST_TOKEN"]
+
+
+def main():
+    with Client(TOKEN) as client:
+        for candle in client.get_all_candles(
+            instrument_id="BBG004730N88",
+            from_=now() - timedelta(days=365),
+            interval=CandleInterval.CANDLE_INTERVAL_HOUR,
+            candle_source_type=CandleSource.CANDLE_SOURCE_UNSPECIFIED,
+        ):
+            print(candle)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
