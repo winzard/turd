9eeae4d1ce047bb38614d754c7d9e2e522458c6f
maksim.vasenkov@eteron.ru <maksim.vasenkov@eteron.ru>
2024-08-01 17:05:57 +0300

    dataframe

--- all_candles.py
+++ all_candles.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
@@ -5,6 +6,8 @@ from datetime import timedelta
 from tinkoff.invest import CandleInterval, Client
 from tinkoff.invest.schemas import CandleSource
 from tinkoff.invest.utils import now
+from pandas import DataFrame
+
 load_dotenv()
 
 TOKEN = os.environ["PROD_INVEST_TOKEN"]
@@ -12,13 +15,18 @@ TOKEN = os.environ["PROD_INVEST_TOKEN"]
 
 def main():
     with Client(TOKEN) as client:
-        for candle in client.get_all_candles(
+        candles = client.get_all_candles(
             instrument_id="BBG004730N88",
-            from_=now() - timedelta(days=365),
-            interval=CandleInterval.CANDLE_INTERVAL_HOUR,
+            from_=now() - timedelta(days=1),
+            interval=CandleInterval.CANDLE_INTERVAL_5_MIN,
             candle_source_type=CandleSource.CANDLE_SOURCE_UNSPECIFIED,
-        ):
-            print(candle)
+        )
+
+        df = DataFrame([{'time': c.time, 'price': Decimal(f'{c.close.units}.{c.close.nano}'), 'volume': c.volume} for c in candles])
+        df['MA_100'] = df['price'].rolling(window=100).mean()    
+        df['MA_12'] = df['price'].rolling(window=12).mean()    
+        print(df)
+        
 
     return 0
 
